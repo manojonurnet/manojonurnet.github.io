56854e1f101637a82f038fc976a00b03de6e0a27
Manoj Neupane <manoj@proshore.eu>
2020-03-15 14:03:19 +0545

    Updates

---
+++ precache-manifest.9532bbb456bd0699c236b56c4a7f1291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4042070f63e6426ccd84a878cf0a1707",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2335b457c5d8e813166",
+    "url": "/portfolio/static/css/main.9cd8a3e1.chunk.css"
+  },
+  {
+    "revision": "f87b65bf9e41d5c99daf",
+    "url": "/portfolio/static/js/2.7e33d66a.chunk.js"
+  },
+  {
+    "revision": "1e3bf01ccf525cade81508e70537c92f",
+    "url": "/portfolio/static/js/2.7e33d66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2335b457c5d8e813166",
+    "url": "/portfolio/static/js/main.7a63e476.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "89654d09842c03a1b871a0967712f897",
+    "url": "/portfolio/static/media/image.89654d09.jpg"
+  }
+]);
